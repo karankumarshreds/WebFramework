ca10e014fd1f1ccd38a6c5d2326320aab2c3e2b9
karankumarshreds <naileditnerd@gmail.com>
2021-06-20 00:40:42 +0530

    event definitions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Karan', age: 20 });
-user.set({ name: 'Karan Kumar' });
+// we can add multiple event handlers on the user
+user.on('change', () => {});
+user.on('change', () => {});
+
+console.log(user);
