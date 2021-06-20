435cdec805dc92610832b79017bea62a5c8a48c3
karankumarshreds <naileditnerd@gmail.com>
2021-06-20 15:31:41 +0530

    add save method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Karan', age: 20 });
-// we can add multiple event handlers on the user
-user.on('change', () => {
-  console.log('change');
-});
-user.on('save', () => {
-  console.log('save');
-});
+const user = new User({ id: 1 });
+user.fetch();
 
-user.trigger('change');
-user.trigger('save');
+console.log(user);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -45,5 +48,25 @@ export class User {
     handlers.forEach((cb) => {
       cb();
     });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
+
+  save(): void {
+    const id = this.get('id');
+    // if the id property is already there that would mean
+    // that the user already exists and we would rather send
+    // a PUT request, and if not, we will send a POST request
+    if (this.get('id')) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post(`http://localhost:3000/users`, this.data);
+    }
   }
 }
